2cd25f16ebd1ce3a19ae77470b013cae98f58f0f
vinicius00franco <vincus.franco.silva@gmail.com>
2025-10-21 20:12:38 -0300

    fix: Correct indentation for ChecklistPage instantiation in MyApp

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -62,7 +62,7 @@ class MyApp extends StatelessWidget {
           ),
           useMaterial3: true,
         ),
-  home: const ChecklistPage(),
+        home: const ChecklistPage(),
       ),
     );
   }
