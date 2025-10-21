fbb1e0dbff1b743aaf7f57c9bbc70835cdc700dc
vinicius00franco <vincus.franco.silva@gmail.com>
2025-10-21 20:12:15 -0300

    fix: Ensure ChecklistPage is instantiated as a constant for improved performance

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -62,7 +62,7 @@ class MyApp extends StatelessWidget {
           ),
           useMaterial3: true,
         ),
-        home: ChecklistPage(),
+  home: const ChecklistPage(),
       ),
     );
   }
